c65ecde08acdf8b42e957c100d79324ad0744992
Aarthi <maarti26932@gmail.com>
2024-05-24 16:01:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const nameRef = useRef('');
+  const emailRef = useRef('');
+  const dobRef = useRef('');
+  const addressRef = useRef('');
+  const [formValue, setForm] = useState([]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const tableData = {
+      name: nameRef.current.value,
+      emailId: emailRef.current.value,
+      dob: dobRef.current.value,
+      address: addressRef.current.value
+    }
+    setForm([...formValue, tableData])
+    nameRef.current.value = '';
+    emailRef.current.value = '';
+    dobRef.current.value = '';
+    addressRef.current.value = '';
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+
+      <h1>FORM USING USEREF </h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" ref={nameRef} placeholder='ENTER YOUR NAME' /> <br />
+        <input type="email" ref={emailRef} placeholder='ENTER EMAIL ID' /><br />
+        <input type="date" ref={dobRef} placeholder='ENTER DOB' /><br />
+        <input type="text" ref={addressRef} placeholder='ENTER ADDRESS' /><br />
+        <button type='submit'>SUBMIT</button><br />
+      </form>
+      <hr />
+      <h1>FORM DATAS IN TABLE</h1>
+      <table >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Age</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {formValue.map((data, index) => {
+            return <tr key={index}>
+              <td>{data.name}</td>
+              <td>{data.emailId}</td>
+              <td>{data.dob}</td>
+              <td>{data.address}</td>
+
+            </tr>
+          })}
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
